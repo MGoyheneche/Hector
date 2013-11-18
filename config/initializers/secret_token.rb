66137020c9cbb6fb6f91180de0ca9139d4d3dc86
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2013-11-18 19:39:24 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hector::Application.config.secret_key_base = '0a1ff0d2ebbb77cad4230f050a9d0dde25980e7de649f27e6a1bece687b3d8a2cdd8b814eeb537dc819141f8ebc37bedc5e82917d1a0c36c25c91d6e930521b8'
