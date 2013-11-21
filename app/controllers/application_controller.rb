85c60055be284b2db3f0d72d6bdd3fccb5d34d9a
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2013-11-21 21:19:00 +0100

    no more bug at sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  protected
+  # protected
 
-  def devise_parameter_sanitizer
-    if resource_class == User
-      User::ParameterSanitizer.new(User, :user, params)
-    else
-      super # Use the default one
-    end
-  end
+  # def devise_parameter_sanitizer
+  #   if resource_class == User
+  #     User::ParameterSanitizer.new(User, :user, params)
+  #   else
+  #     super # Use the default one
+  #   end
+  # end
 end
